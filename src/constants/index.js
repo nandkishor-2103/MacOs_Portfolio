912aa09e868d889d9a1bef85fc2d8f6710334d45
nandkishor-2103 <mandalnandkishorbk@gmail.com>
2026-01-27 20:08:29 +0530

    implement a safari component to show blogs post

--- src/constants/index.js
+++ src/constants/index.js
@@ -83,21 +83,21 @@ const blogPosts = [
     title:
       'TypeScript Explained: What It Is, Why It Matters, and How to Master It',
     image: '/images/blog1.png',
-    link: 'https://jsmastery.com/blog/typescript-explained-what-it-is-why-it-matters-and-how-to-master-it',
+    link: 'https://typescript.tv/blog/',
   },
   {
     id: 2,
     date: 'Aug 28, 2025',
     title: 'The Ultimate Guide to Mastering Three.js for 3D Development',
     image: '/images/blog2.png',
-    link: 'https://jsmastery.com/blog/the-ultimate-guide-to-mastering-three-js-for-3d-development',
+    link: 'https://threejsresources.com/blog',
   },
   {
     id: 3,
     date: 'Aug 15, 2025',
     title: 'The Ultimate Guide to Mastering GSAP Animations',
     image: '/images/blog3.png',
-    link: 'https://jsmastery.com/blog/the-ultimate-guide-to-mastering-gsap-animations',
+    link: '',
   },
 ];
 
